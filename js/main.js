a77186f960ab790e77d383761c6ae164e0fcf812
Katarzyna <figarskak@gmail.com>
2018-03-03 18:11:52 +0100

    warsztat

---
+++ js/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+//console.log("test");
+
+//    $("#carouselExampleControls").carousel({
+//        interval: 2000
+//    });  sami ustalamy interval ale jeśli staniemy na obrazku zatrzymuje się
+// 
